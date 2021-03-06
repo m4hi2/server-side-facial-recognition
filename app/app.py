5245dc1d9bae851c1d3f6bd5c8bab49ef416d37b
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-03-06 06:12:03 +0600

    feat: video stream from browrser round trip
    1. Video Input taken from webcam with web browser
    2. Video stream sent to server from browser with sockets
    3. Video received in server with socket
    4. Video processed in server
    5. Processed Video sent back to browser

---
+++ app/app.py
@@ -0,0 +1,54 @@
+import base64
+import io
+import random
+import string
+from io import StringIO
+
+import cv2
+import imutils
+import numpy as np
+from flask import Flask, render_template
+from flask.wrappers import Response
+from flask_socketio import SocketIO, emit
+from PIL import Image
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@socketio.on('image')
+def image(data_image):
+    subf = StringIO()
+    subf.write(data_image)
+
+    # decode image and convert into image
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    # converting RGB to BGR, as openCV standards
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+    # process the image frames
+
+    frame = imutils.resize(frame, width=700)
+    frame = cv2.flip(frame, 1)
+
+    _, imgencode = cv2.imencode(".jpg", frame)
+
+    # base64 encode
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpeg;base64,'
+
+    stringData = b64_src + stringData
+
+    emit('response_back', stringData)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1')
